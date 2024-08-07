643f4a8a4a5b65aa6350be74d678c727be566b74
LAPTOP-UMM7HUUJ\user <ntuananh2897@gmail.com>
2024-08-07 22:15:38 +0700

    feat: add progress bar when change page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import { SettingsProvider, useSettingsContext } from "@/contexts/settingsContext";
 import { store } from "@/stores";
-import "@/styles/globals.css";
+import '@/utils/routingLoader'
+import "@/styles/index.scss";
 import ThemeComponent from "@/theme/ThemeComponent";
 import type { AppProps } from "next/app";
 import { Provider } from 'react-redux'
 
 export default function App({ Component, pageProps }: AppProps) {
   const { settings } = useSettingsContext();
+
   return (
     <Provider store={store}>
       <SettingsProvider>
---
+++ src/utils/routingLoader.ts
@@ -0,0 +1,13 @@
+import { Router } from "next/router";
+import NProgress from 'nprogress'
+
+
+Router.events.on('routeChangeStart', () => {
+    NProgress.start()
+  })
+  Router.events.on('routeChangeError', () => {
+    NProgress.done()
+  })
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done()
+  })
